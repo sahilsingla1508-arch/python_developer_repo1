cbe29c54082b4162db9c137ab530eef89cbcd0d9
sahilsingla1508-arch <sahilsingla1508@gmail.com>
2026-07-12 12:26:56 +0530

    Save local changes

---
+++ sample.py
@@ -0,0 +1,13 @@
+# Simple Python Program
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = 10
+num2 = 20
+
+result = add_numbers(num1, num2)
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+print("Sum =", result)

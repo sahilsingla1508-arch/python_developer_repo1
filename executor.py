0ce0e5b6132de3436075d7d5735c07fbbb42a41f
VaradKod <varadkod2023@gmail,com>
2026-07-15 23:27:18 +0530

    Day 1: Load Python source file

--- executor.py
+++ executor.py
@@ -8,11 +8,21 @@ class PythonExecutor:
     def file_exists(self):
         return os.path.exists(self.filename)
 
+    def load_source(self):
+        with open(self.filename, "r", encoding="utf-8") as file:
+            return file.read()
+
 
 if __name__ == "__main__":
     executor = PythonExecutor("sample.py")
 
     if executor.file_exists():
-        print("File exists")
+        source = executor.load_source()
+
+        print("=" * 40)
+        print("Source Code Loaded Successfully")
+        print("=" * 40)
+        print(source)
+
     else:
-        print("File not found")
+        print("File not found.")

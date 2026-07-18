2c9b51ec22da77b496ee615056835bd468868664
Tejas <tejasnair6895@gmail.com>
2026-07-18 16:08:42 +0530

    Save work before merge

--- sample2.py
+++ sample2.py
@@ -1,4 +1,14 @@
-total = 0
-for i in range(5):
-    total = total + i
-print(total)
+# total = 0
+# for i in range(5):
+#     total = total + i
+# print(total)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    status = "Adult"
+else:
+    status = "Minor"
+
+print(f"Hello {name}")
